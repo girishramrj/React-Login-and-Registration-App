65418e50dab2b91c728560647ee60e0294181928
girishramrj <128630449+girishramrj@users.noreply.github.com>
2024-10-24 11:36:31 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://13.48.190.30:5000/api/login', { username, password }); //Your desired Address
+      const res = await axios.post('http://13.48.190.30:5000/api/login', { username, password }); //Change to Your desired Address
       const { user } = res.data;
       localStorage.setItem('username', user.username);
       navigate('/profile');
